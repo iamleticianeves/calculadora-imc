a6507809f80e38f1e191f8dbea60b1094f368674
Letícia Neves <iamleticianeves@gmail.com>
2023-08-19 23:41:58 -0300

    first commit!

--- scripts.js
+++ scripts.js
@@ -56,16 +56,10 @@ document.querySelector("#result").innerHTML = imc.toFixed(2);
 
 }
 
-
-
-
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
